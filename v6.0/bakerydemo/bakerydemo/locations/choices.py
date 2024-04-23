803a6a98c6f51c9ba115b8d87b7d1a6d13bc81bc
Thibaud Colas <thibaudcolas@gmail.com>
2024-04-23 08:53:58 +0100

    Add v6.0

---
+++ v6.0/bakerydemo/bakerydemo/locations/choices.py
@@ -0,0 +1,9 @@
+DAY_CHOICES = (
+    ("MON", "Monday"),
+    ("TUE", "Tuesday"),
+    ("WED", "Wednesday"),
+    ("THU", "Thursday"),
+    ("FRI", "Friday"),
+    ("SAT", "Saturday"),
+    ("SUN", "Sunday"),
+)
